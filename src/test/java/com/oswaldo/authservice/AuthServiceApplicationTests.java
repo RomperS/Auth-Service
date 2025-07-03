e764c802e15a312a4d94a0b3184fe09c77552484
RomperS <Oswaldolo663@gmail.com>
2025-07-03 10:18:02 -0500

    feat: initial commit

---
+++ src/test/java/com/oswaldo/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oswaldo.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
